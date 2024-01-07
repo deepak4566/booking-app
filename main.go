70ae3f23ca6ed1dfe42d1b83df83daf16997e15a
deepak4566 <deepakreddyats1@gmail.com>
2024-01-07 17:16:39 +0530

    added go files

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var conferenceName string = "Goconference"
+
+const conferenceTickets uint = 50
+
+var remainingTickets uint = 50
+
+func main() {
+	
+	var firstName string
+	var lastName string
+	var email string
+	var TicketsByUser uint
+
+for{
+
+    fmt.Printf("welcome to our %v and total tickets in conference is %v \n", conferenceName, conferenceTickets)
+
+
+	fmt.Println("Please enter your first name:")
+	fmt.Scanln(&firstName)
+
+	fmt.Println("Please enter your last name:")
+	fmt.Scanln(&lastName)
+
+	fmt.Println("Please enter your email:")
+	fmt.Scanln(&email)
+
+	fmt.Println("Please enter no of tickets:")
+	fmt.Scanln(&TicketsByUser)
+
+	remainingTickets = remainingTickets - TicketsByUser
+	fmt.Printf("remaining tickets beforeis %v\n", remainingTickets)
+
+	ifFirstName := len(firstName) > 2
+	ifLastName := len(lastName) > 2
+	ifEmail := strings.Contains(email, "@")
+
+	ifTicketsCond := TicketsByUser <= remainingTickets && TicketsByUser > 0
+
+	if ifFirstName && ifLastName && ifEmail && ifTicketsCond {
+		fmt.Printf("Hello %v %v, you have successfully registered for the conference\n", firstName, lastName)
+		fmt.Printf("remaining tickets after is %v\n", remainingTickets)
+	} else {
+		if !ifFirstName || !ifLastName {
+			fmt.Println("Please enter valid name with more than 2 characters")
+		}
+		if !ifEmail {
+			fmt.Println("Please enter valid email")
+		}
+
+		if !ifTicketsCond {
+			fmt.Println("Please enter valid no of  tickets")
+		}
+	}
+}
+
+
+
