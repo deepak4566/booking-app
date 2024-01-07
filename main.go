96c014647ae863b5d277b3666d58885ce08a2cb4
deepak4566 <deepakreddyats1@gmail.com>
2024-01-07 17:53:12 +0530

    changed order

--- main.go
+++ main.go
@@ -12,54 +12,58 @@ const conferenceTickets uint = 50
 var remainingTickets uint = 50
 
 func main() {
-	
+
 	var firstName string
 	var lastName string
 	var email string
 	var TicketsByUser uint
 
-for{
+	for {
 
-    fmt.Printf("welcome to our %v and total tickets in conference is %v \n", conferenceName, conferenceTickets)
+		fmt.Printf("welcome to our %v and total tickets in conference is %v \n", conferenceName, conferenceTickets)
 
+		fmt.Println("Please enter your first name:")
+		fmt.Scanln(&firstName)
 
-	fmt.Println("Please enter your first name:")
-	fmt.Scanln(&firstName)
+		fmt.Println("Please enter your last name:")
+		fmt.Scanln(&lastName)
 
-	fmt.Println("Please enter your last name:")
-	fmt.Scanln(&lastName)
+		fmt.Println("Please enter your email:")
+		fmt.Scanln(&email)
 
-	fmt.Println("Please enter your email:")
-	fmt.Scanln(&email)
+		fmt.Println("Please enter no of tickets:")
+		fmt.Scanln(&TicketsByUser)
 
-	fmt.Println("Please enter no of tickets:")
-	fmt.Scanln(&TicketsByUser)
+		remainingTickets = remainingTickets - TicketsByUser
 
-	remainingTickets = remainingTickets - TicketsByUser
-	fmt.Printf("remaining tickets beforeis %v\n", remainingTickets)
+		if remainingTickets == 0 {
+			fmt.Println("No more tickets available. The conference is sold out.")
+			break
+		}
 
-	ifFirstName := len(firstName) > 2
-	ifLastName := len(lastName) > 2
-	ifEmail := strings.Contains(email, "@")
+		ifFirstName := len(firstName) > 2
+		ifLastName := len(lastName) > 2
+		ifEmail := strings.Contains(email, "@")
 
-	ifTicketsCond := TicketsByUser <= remainingTickets && TicketsByUser > 0
+		ifTicketsCond := TicketsByUser <= remainingTickets
 
-	if ifFirstName && ifLastName && ifEmail && ifTicketsCond {
-		fmt.Printf("Hello %v %v, you have successfully registered for the conference\n", firstName, lastName)
+		if ifFirstName && ifLastName && ifEmail && ifTicketsCond {
+			fmt.Printf("Hello %v %v, you have successfully registered for the conference\n", firstName, lastName)
+
+		} else {
+			if !ifFirstName || !ifLastName {
+				fmt.Println("Please enter valid name with more than 2 characters")
+			}
+			if !ifEmail {
+				fmt.Println("Please enter valid email")
+			}
+
+			if !ifTicketsCond {
+				fmt.Println("Please enter valid no of  tickets")
+			}
+		}
+
 		fmt.Printf("remaining tickets after is %v\n", remainingTickets)
-	} else {
-		if !ifFirstName || !ifLastName {
-			fmt.Println("Please enter valid name with more than 2 characters")
-		}
-		if !ifEmail {
-			fmt.Println("Please enter valid email")
-		}
-
-		if !ifTicketsCond {
-			fmt.Println("Please enter valid no of  tickets")
-		}
 	}
+
 }
-
-
-
